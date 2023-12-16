55f3e5a4435e7da84b970cd773eb798b07f0d04d
balu909066 <tempmailbalu@gmail.com>
2023-12-16 14:16:51 +0530

    Hello

---
+++ src/main/java/in/kmit/Project2/App.java
@@ -0,0 +1,13 @@
+package in.kmit.Project2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Balu" );
+    }
+}
